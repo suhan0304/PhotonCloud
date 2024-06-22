40f8cf1f8edb700c36f116b03cb54ad479b6d274
ZerrY <suhan0304@gmail.com>
2024-06-22 15:43:30 +0900

    Fix Error

--- StudyProject/Assets/Scripts/GameManager.cs
+++ StudyProject/Assets/Scripts/GameManager.cs
@@ -1,19 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 using TMPro;
 using UnityEngine.UI;
-using Photon.Realtime;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviourPunCallbacks
 {
     public TMP_Text roomName;
     public TMP_Text connectInfo;
     public TMP_Text msgList;
+
     public Button exitBtn;
 
-    void Awake() {
+    void Awake()
+    {
         CreatePlayer();
         // 접속 정보 추출 및 표시
         SetRoomInfo();
@@ -21,24 +22,40 @@ public class GameManager : MonoBehaviourPunCallbacks
         exitBtn.onClick.AddListener(() => OnExitClick());
     }
 
-    void CreatePlayer() {
+    void CreatePlayer()
+    {
+        // 출현 위치 정보를 배열에 저장
         Transform[] points = GameObject.Find("SpawnPointGroup").GetComponentsInChildren<Transform>();
         int idx = Random.Range(1, points.Length);
 
-        PhotonNetwork.Instantiate("Player", points[idx].position, points[idx].rotation, 0);
+        // 네트워크상에 캐릭터 생성
+        PhotonNetwork.Instantiate("Player",
+                                  points[idx].position,
+                                  points[idx].rotation,
+                                  0); 
     }
 
-    void SetRoomInfo() {
+    // 룸 접속 정보를 출력
+    void SetRoomInfo()
+    {
         Room room = PhotonNetwork.CurrentRoom;
         roomName.text = room.Name;
         connectInfo.text = $"({room.PlayerCount}/{room.MaxPlayers})";
     }
 
-    private void OnExitClick() {
+    // Exit 버튼의 OnClick에 연결할 함수
+    private void OnExitClick()
+    {
         PhotonNetwork.LeaveRoom();
     }
 
-    // 룸으로 새로운 네트워크 유저가 접속했을때 콜백 함수
+    // 포톤 룸에서 퇴장했을 때 호출되는 콜백함수
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+
+    // 룸으로 새로운 네트워크 유저가 입장했때 호출되는 콜백함수
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
         SetRoomInfo();
@@ -46,8 +63,9 @@ public class GameManager : MonoBehaviourPunCallbacks
         msgList.text += msg;
     }
 
-    // 룸에서 네트워크 유저가 퇴장했을때 호출되는 콜백함수
-    public override void OnPlayerLeftRoom(Player otherPlayer){
+    // 룸에서 네트워크 유저가 퇴장했때 호출되는 콜백함수
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
         SetRoomInfo();
         string msg = $"\n<color=#ff0000>{otherPlayer.NickName}</color> is left room";
         msgList.text += msg;
